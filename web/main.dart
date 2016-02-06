0d90b7285784aa26a68d9cb15fbf3b2a3a48d162
Kasper Peulen <kasperpeulen@gmail.com>
2016-02-07 00:11:12 +0100

    feat(): login using firebase

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,32 @@
+import 'dart:async';
+
 import 'package:angular2/bootstrap.dart';
+import 'package:angular2/angular2.dart';
 import 'package:the_zone/components/body.dart';
 import 'package:the_zone/services/my_service.dart';
+import 'package:firebase/firebase.dart';
+import 'package:github/browser.dart';
+import 'package:the_zone/services/user_service.dart';
 
-void main() {
-  bootstrap(BodyComponent, [MyService]);
+Future main() async {
+  final firebase = new Firebase('http://mathedit.firebaseio.com/');
+
+  bootstrap(BodyComponent, [
+    MyService,
+    UserService,
+    provide(Firebase, useValue: firebase),
+    provide(Authentication, useValue: await bootstrapAuth(firebase)),
+    provide(GitHub,
+        useFactory: (Authentication auth) => createGitHubClient(auth: auth),
+        deps: [Authentication]),
+  ]);
+}
+
+Future<Authentication> bootstrapAuth(Firebase firebase) async {
+  final authJson = await firebase.onAuth().first;
+  if (authJson != null && authJson['provider'] == 'github') {
+    return new Authentication.withToken(authJson['github']['accessToken']);
+  } else {
+    return new Authentication.anonymous();
+  }
 }
