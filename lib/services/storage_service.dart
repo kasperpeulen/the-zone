c778fd3f95193f508da4a89407723988ba832c48
Kasper Peulen <kasperpeulen@gmail.com>
2016-02-08 23:56:20 +0100

    fix(): one bug fix and some refactorings
    added toList() to make sure it is no Iterable

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/core.dart';
 import 'package:firebase/firebase.dart';
-import 'package:github/browser.dart';
+import 'package:github/browser.dart' hide Event;
 import 'package:the_zone/models.dart';
 
 @Injectable()
@@ -10,36 +10,31 @@ class StorageService {
   List<TimeRecord> recordings = [];
 
   StorageService(this._firebase, this._auth) {
-    if(_auth.isToken) {
-      _onChildAdded();
-      _onChildChanged();
-      _onChildRemoved();
+    if (_auth.isToken) {
+      _recordingsChild.onChildAdded.listen(_onChildAdded);
+      _recordingsChild.onChildChanged.listen(_onChildChanged);
+      _recordingsChild.onChildRemoved.listen(_onChildRemoved);
     }
   }
 
-  Firebase _recordingsChild() {
+  Firebase get _recordingsChild {
     final auth = _firebase.getAuth();
     return _firebase.child("users").child(auth['uid']).child('recordings');
   }
 
-  void _onChildAdded() {
-    _recordingsChild().onChildAdded.listen((e) {
-      recordings.add(new TimeRecord.fromJson(e.snapshot.val()));
-    });
+  void _onChildAdded(Event e) {
+    recordings.add(new TimeRecord.fromJson(e.snapshot.val()));
   }
 
-  void _onChildChanged() {
-    _recordingsChild().onChildChanged.listen((e) {
-      final recording = new TimeRecord.fromJson(e.snapshot.val());
-      recordings = recordings.map((r) => r.uid == recording.uid ? recording : r);
-    });
+  void _onChildChanged(Event e) {
+    final recording = new TimeRecord.fromJson(e.snapshot.val());
+    recordings =
+        recordings.map((r) => r.uid == recording.uid ? recording : r).toList();
   }
 
-  void _onChildRemoved() {
-    _recordingsChild().onChildRemoved.listen((e) {
-      final recording = new TimeRecord.fromJson(e.snapshot.val());
-      recordings.removeWhere((r) => recording.uid == r.uid);
-    });
+  void _onChildRemoved(Event e) {
+    final recording = new TimeRecord.fromJson(e.snapshot.val());
+    recordings.removeWhere((r) => recording.uid == r.uid);
   }
 
   void update(TimeRecord recording) {
@@ -53,15 +48,12 @@ class StorageService {
   }
 
   void push(TimeRecord recording) {
-    final auth = _firebase.getAuth();
-    final ref =
-        _firebase.child('users').child(auth['uid']).child('recordings').push();
-
+    final ref = _recordingsChild.push();
     recording.uid = ref.key;
     ref.set(recording.toJson());
   }
 
-  void reset(){
-    _recordingsChild().set([]);
+  void reset() {
+    _recordingsChild.set([]);
   }
 }
